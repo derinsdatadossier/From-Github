beb5e2c58a780bb49f2df5e31c866d2d1f710646
philson <philson11113@gmail.com>
2025-02-11 00:15:47 +0100

    created methods and added comments

--- test.py
+++ test.py
@@ -1,5 +1,9 @@
 class Person:
-    pass
+    def display(self):      #creating methods (functions inside classes)
+        print("I am a person")
 
-p1 = Person()
+    def greet(self):
+        print("Hello, how are you doing?")
+
+p1 = Person() #instatitating the class
 p2 = Person()
