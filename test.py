0fe2dde4d9b30a0f188b0a1e8f722ec2cd0ecca8
philson <philson11113@gmail.com>
2025-02-19 19:25:19 +0100

    made instance variables dynamic and added name&age parameters

--- test.py
+++ test.py
@@ -1,19 +1,24 @@
 class Person:
-    def set_details(self):
-        self.name = "John" #instance variables
-        self.age = 20
+    def set_details(self,name,age): #self refers to the current instance
+        self.name = name #instance variables
+        self.age = age
+# self.age and self.name are 2 instance variables that can be used anywhere in the class
+# name and age in line 2 are parameters of the method local variables that can only be used in method
 
     def display(self):      #creating methods (functions inside classes)
-        print("I am a person")
+        print("Hi my name is ",self.name)
 
     def greet(self):
-        print("Hello, how are you doing?")
+        if self.age<80:
+            print("Hello, how are you doing?")
+        else:
+            print("Hello, how do you do")
 
 p1 = Person() #instatitating the class
 p2 = Person()
 
-p1.set_details() #each instance has it's own copy of instance variables
-p2.set_details()
+p1.set_details("Bob",32) #each instance has it's own copy of instance variables
+p2.set_details("Rob",30)
 
 #now call methods
 p1.greet()
