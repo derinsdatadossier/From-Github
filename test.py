f0900d24e8c3cfe0259804f66fea7b51643a28f3
philson <philson11113@gmail.com>
2025-02-19 23:39:53 +0100

    called self.display() in greet method

--- test.py
+++ test.py
@@ -6,14 +6,14 @@ class Person:
 # name and age in line 2 are parameters of the method local variables that can only be used in method
 
     def display(self):      #creating methods (functions inside classes)
-        print("Hi my name is ",self.name)
+        print("My name is ",self.name)
 
     def greet(self):
         if self.age<80:
             print("Hello, how are you doing?")
         else:
             print("Hello, how do you do")
-
+        self.display()
 p1 = Person() #instatitating the class
 p2 = Person()
 
@@ -22,7 +22,5 @@ p2.set_details("Rob",30)
 
 #now call methods
 p1.greet()
-p1.display()
-
 p2.greet()
-p2.display()
+
