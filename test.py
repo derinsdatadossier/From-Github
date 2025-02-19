8195cca755330b8e2d48bc96536a4c640a84fdff
philson <philson11113@gmail.com>
2025-02-19 18:47:05 +0100

    added self in lines 3 and 6

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 class Person:
     def display(self):      #creating methods (functions inside classes)
-        print("I am a person")
+        print("I am a person",self)
 
     def greet(self):
-        print("Hello, how are you doing?")
+        print("Hello, how are you doing?",self)
 
 p1 = Person() #instatitating the class
 p2 = Person()
